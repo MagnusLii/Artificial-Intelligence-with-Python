6705b164ff49918f3b55f409602795c01033b411
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-06-20 13:39:37 +0300

    Update main.py
    Final version.

--- 53_Car_mpg/main.py
+++ 53_Car_mpg/main.py
@@ -8,7 +8,6 @@ from sklearn.linear_model import Ridge, Lasso
 
 # Task 1: Read the data into pandas dataframe
 df = pandas.read_csv("53_Car_mpg\Auto.csv")
-columns = ["cylinders", "displacement", "horsepower", "weight", "acceleration", "year"] #TODO Obsolete! Delete later.
 
 
 # Task 2: Setup multiple regression X and y
@@ -16,7 +15,7 @@ x = df.drop(["mpg", "name", "origin"], axis=1)
 y = df["mpg"]
 
 # Task 3: Split data into training and testing sets (80/20 split)
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 5)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 5)
 
 # Task 4: Implement both ridge regression and LASSO regression using several values for alpha
 alphas = numpy.linspace(0, 1000, 50)
@@ -26,14 +25,14 @@ lasso_scores = []
 for alpha in alphas:
     # ridge Regression
     ridge = Ridge(alpha=alpha)
-    ridge.fit(X_train, y_train)
-    ridge_pred = ridge.predict(X_test)
+    ridge.fit(x_train, y_train)
+    ridge_pred = ridge.predict(x_test)
     ridge_scores.append(r2_score(y_test, ridge_pred))
 
     # lasso Regression
     lasso = Lasso(alpha=alpha)
-    lasso.fit(X_train, y_train)
-    lasso_pred = lasso.predict(X_test)
+    lasso.fit(x_train, y_train)
+    lasso_pred = lasso.predict(x_test)
     lasso_scores.append(r2_score(y_test, lasso_pred))
 
 # Task 5: Search optimal value for alpha (in terms of R2 score)
